98acc2d39d35566fadabbd77ecf903f7d68d6935
Donald Gray <donald.gray@digirati.com>
2021-12-20 14:55:53 +0000

    Add logout function

--- main.py
+++ main.py
@@ -6,28 +6,29 @@ from flask_session import Session
 from flask_caching import Cache
 from flask_httpauth import HTTPBasicAuth
 
-SESSION_KEY = 'session_user'
-ACCESS_KEY = os.environ.get('ACCESS_KEY')
-ACCESS_SECRET = os.environ.get('ACCESS_SECRET')
+SESSION_KEY = "session_user"
+ACCESS_KEY = os.environ.get("ACCESS_KEY")
+ACCESS_SECRET = os.environ.get("ACCESS_SECRET")
 
 auth = HTTPBasicAuth()
 
-app = Flask('dlcs-role-provider')
+app = Flask("dlcs-role-provider")
 
 config = {
-    'DEBUG': True,
-    'CACHE_TYPE': 'SimpleCache',
-    'CACHE_DEFAULT_TIMEOUT': 300,
-    'SESSION_TYPE': 'filesystem',
-    'SESSION_PERMANENT': False,
-    'SESSION_USE_SIGNER': True,
-    'SESSION_KEY_PREFIX': 'dlcs_rp:',
-    'SESSION_FILE_THRESHOLD': 100,
+    "DEBUG": True,
+    "CACHE_TYPE": "SimpleCache",
+    "CACHE_DEFAULT_TIMEOUT": 600,
+    "CACHE_THRESHOLD": 100,
+    "SESSION_TYPE": "filesystem",
+    "SESSION_PERMANENT": False,
+    "SESSION_USE_SIGNER": True,
+    "SESSION_KEY_PREFIX": "dlcs_rp:",
+    "SESSION_FILE_THRESHOLD": 100,
 }
 app.config.from_mapping(config)
 
 # flask_session
-app.secret_key = os.environ.get('FLASK_SECRET')
+app.secret_key = os.environ.get("FLASK_SECRET")
 Session(app)
 
 # flask_cache
@@ -38,12 +39,12 @@ settings = {}
 
 @app.before_first_request
 def startup():
-    with open('settings.json') as settings_json:
+    with open("settings.json") as settings_json:
         global settings
         settings = json.load(settings_json)
 
 
-@app.route("/login", methods=['GET'])
+@app.route("/login", methods=["GET"])
 def login():
     """
     Handle GET requests for login page.
@@ -52,13 +53,13 @@ def login():
 
     # if user has an active session, redirect to dlcs
     if session_user := session.get(SESSION_KEY, {}):
-        return _redirect_to_dlcs(session_user['dlcs_root'], session_user['customer'], session_user['token'])
+        return _redirect_to_dlcs(session_user)
 
     # else render login page
     return _render_login()
 
 
-@app.route("/login", methods=['POST'])
+@app.route("/login", methods=["POST"])
 def do_login():
     """
     Handle POST requests for login page.
@@ -66,31 +67,32 @@ def do_login():
     Once authenticated, user is logged in and a session is established containing accessible roles.
     """
 
-    username = request.form['username']
-    password = request.form['password']
-    dlcs_root = request.form['dlcs'] or settings['dlcs-default-root']
-    dlcs_customer = request.form['customer'] or settings['dlcs-customer']
+    username = request.form["username"]
+    password = request.form["password"]
+    dlcs_root = request.form["dlcs"] or settings["dlcs-default-root"]
+    dlcs_customer = request.form["customer"] or settings["dlcs-customer"]
 
     success, user_roles = _verify_auth(username, password)
 
     if success:
         # create session
         session_key = str(uuid.uuid4())
-        session[SESSION_KEY] = {
+        session_user = {
             "token": session_key,
             "dlcs_root": dlcs_root,
             "roles": user_roles,
             "customer": dlcs_customer
         }
+        session[SESSION_KEY] = session_user
 
         # cache user-roles for later retrieval
         cache.set(_get_roles_cache_key(session_key), user_roles)
 
         # redirect to DLCS with a session id
-        return _redirect_to_dlcs(dlcs_root, dlcs_customer, session_key)
+        return _redirect_to_dlcs(session_user)
     else:
         # re-render template with an error
-        return _render_login('Unknown Credentials')
+        return _render_login(error="Unknown Credentials")
 
 
 @app.route("/roles")
@@ -108,28 +110,45 @@ def roles():
     abort(404, description="Resource not found")
 
 
+@app.route("/logout", methods=["GET"])
+def logout():
+    """
+    Remove user session.
+    This can be helpful if session exists but cached roles do not.
+    """
+    if session_user := session.get(SESSION_KEY, {}):
+        cache.delete(_get_roles_cache_key(session_user["token"]))
+
+    session[SESSION_KEY] = {}
+    return _render_login(message="User logged out")
+
+
 @auth.verify_password
 def verify_user(username, password):
     return username == ACCESS_KEY and password == ACCESS_SECRET
 
 
-def _render_login(error=None):
-    return render_template('login.html',
-                           default_customer=settings['dlcs-customer'],
-                           default_root=settings['dlcs-default-root'],
-                           users=settings['users'],
-                           error=error)
+def _render_login(error=None, message=None):
+    return render_template("login.html",
+                           default_customer=settings["dlcs-customer"],
+                           default_root=settings["dlcs-default-root"],
+                           users=settings["users"],
+                           error=error,
+                           message=message)
 
 
 def _verify_auth(username: str, password: str):
-    for user in settings['users']:
-        if user['username'] == username and user['password'] == password:
-            return True, user['roles']
+    for user in settings["users"]:
+        if user["username"] == username and user["password"] == password:
+            return True, user["roles"]
 
     return False, []
 
 
-def _redirect_to_dlcs(dlcs_root, dlcs_customer, session_key):
+def _redirect_to_dlcs(session_user: dict):
+    dlcs_root = session_user["dlcs_root"]
+    dlcs_customer = session_user["customer"]
+    session_key = session_user["token"]
     return redirect(f"https://{dlcs_root}/auth/{dlcs_customer}/fromcas?token={session_key}")
 
 
--- readme.md
+++ readme.md
@@ -1,3 +1,20 @@
 # Role Provider Demonstrator
 
 Basic app for demonstrating DLCS role-provider functionality.
+
+## Overview
+
+This is a basic app that shows the minimum functionality for a role-provider with no downstream dependency.
+
+## Tech :robot:
+
+* [Flask](https://flask.palletsprojects.com)
+* [Flask-Session](https://flask-session.readthedocs.io/en/latest/)
+* [Flask-Caching](https://flask-caching.readthedocs.io/en/latest/)
+* [Flask-HttpAuth](https://flask-httpauth.readthedocs.io/en/latest/)
+
+## Limitations
+
+Session storage is on disk so sessions can be lost of underlying hardware replaced.
+
+Caching is in-memory, stored sessions will be lost if service restarted.
