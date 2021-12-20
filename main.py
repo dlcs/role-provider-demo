91a453b01b57b6e5a4114bf3cd3e1af0c14ea026
Donald Gray <donald.gray@digirati.com>
2021-12-20 08:05:52 +0000

    Read settings file

--- main.py
+++ main.py
@@ -1,11 +1,21 @@
+import json
 from flask import Flask, render_template
 
 app = Flask("dlcs-role-provider")
 
+settings = {}
+
+
+@app.before_first_request
+def startup():
+    with open('settings.json') as settings_json:
+        global settings
+        settings = json.load(settings_json)
+
 
 @app.route("/login", methods=['GET'])
 def login():
-    return render_template("login.html")
+    return render_template('login.html', default_root=settings['dlcs-default-root'], users=settings['users'])
 
 
 @app.route("/info")
